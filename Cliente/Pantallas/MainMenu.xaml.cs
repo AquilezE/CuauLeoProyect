07c6994d532bee67f09458ad072b812972b871c9
Cuauhtemoc Calderon <cuahu3000@gmail.com>
2024-09-26 20:30:07 -0600

    Hice la ventana de Main menu , Falta agregar iconos de Configuracion y Ver perfil

---
+++ Cliente/Pantallas/MainMenu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cliente.Pantallas
+{
+    /// <summary>
+    /// Lógica de interacción para MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btCreateLobby_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btJoinLobby_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btFriends_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btStats_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btLogOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
