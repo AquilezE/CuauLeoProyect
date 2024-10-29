89e28f2623843bf29e246d80ee8bd8bd82d62398
Cuauhtemoc Calderon <cuahu3000@gmail.com>
2024-10-28 22:02:12 -0600

    Made the fisrst version of Social singleton and implemented the serialization on friend to get data from the service

---
+++ Cliente/Social.cs
@@ -0,0 +1,57 @@
+﻿using Cliente.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    internal class Social: ISocialManagerCallback
+    {
+        private static SocialManagerClient socialManagerClient;
+
+        public static Social instance;
+        public ObservableCollection<Friend> friendList { get; set; }
+
+        public Social()
+        {
+            socialManagerClient = new SocialManagerClient(new System.ServiceModel.InstanceContext(this));
+            friendList = new ObservableCollection<Friend>();
+        }
+
+        public static Social Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Social();
+                }
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+
+        public void GetFriends()
+        {
+            FriendDTO[] friends = socialManagerClient.GetFriends(User.Instance.ID);
+            foreach (FriendDTO friend in friends)
+            {
+                Console.WriteLine(friend.FriendName);
+                friendList.Add(new Friend(friend));
+            }
+
+        }
+
+        public void OnFriendNew(FriendDTO[] friends)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
